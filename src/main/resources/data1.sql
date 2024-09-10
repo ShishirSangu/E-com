e4c8f0df056442e69416a6c5c4a6d58c157ab8ab
Your Name <sangameshshishir@gmail.com>
2024-09-10 23:35:20 +0530

    first commit

---
+++ src/main/resources/data1.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+VALUES
+('Tata Nexon', 'Features and performance', 'Tata Motors', 750000.99, 'Cars', '2024-01-15', true, 45),
+('Renault Kwid', 'Fuel efficiency and reliable', 'Renault Motors', 500000.00, 'Cars', '2018-02-01', false, 100),
+('BMW X5', 'Luxury SUV with advanced technology', 'BMW', 9000000.50, 'Cars', '2022-05-20', true, 25),
+('Mercedes-Benz C-Class', 'Elegant design with powerful performance', 'Mercedes-Benz', 7000000.75, 'Cars', '2023-07-10', true, 30),
+('Audi Q7', 'Spacious and comfortable with premium features', 'Audi', 8500000.00, 'Cars', '2021-03-15', true, 20),
+('Ford Mustang', 'Iconic muscle car with thrilling performance', 'Ford', 6000000.99, 'Cars', '2020-09-30', true, 15);
